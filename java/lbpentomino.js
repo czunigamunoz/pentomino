2bae9da4ba21df7e0a54227ea06b89c03f68cc39
czm <cezunigamunoz@gmail.com>
2020-08-05 22:01:12 -0500

    1.1

--- java/lbpentomino.js
+++ java/lbpentomino.js
@@ -1,36 +1,54 @@
+const width = 13;
+const height = 10;
+
+
+
+function drawDropZone(){
+    let dropZone = document.getElementById("drop-zone");
+    for(let i=0; i<width*height; i++){
+        dropZone.appendChild(document.createElement("div"));
+    }
+}
+
 function draw(){
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    img = new Image();
+    const img = new Image();
     img.onload = () => {
         ctx.fillStyle = 'red';
-        ctx.fillRect(10,10,70,50);
-        ctx.fillRect(40,60,70, 60);
+        ctx.fillRect(0,50,150,50);
+        ctx.fillRect(50,0,50,50);
+        ctx.fillRect(50,100,50,50);
     }
     img.src = 'img/rojo.png';
+}
+
+function hola(){
     console.log(img);
-}
-
-function drag(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    e.dataTransfer.setDragImage(img, 100, 100);  
-}
-
-function drop(e){
-    var imgDrop = e.dataTransfer.getData('text');
-    e.target.appendChild(document.getElementById(imgDrop));
-}
-
-function inicio(){
-    draw();
-    ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( (e) => {
+/*     ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( (e) => {
         img.addEventListener(event, (e) => {
             e.preventDefault();
             e.stopPropagation();
         });
-    });
-    img.addEventListener('drop', drop, false);
+    }); */
     img.addEventListener('dragstart', drag, false);
+    img.addEventListener('drop', drop, false);
+    console.log("hola");
 }
 
-window.onload=inicio;
+
+function drag(e) {
+    e.preventDefault();
+    console.log(e);
+}
+
+function drop(e){
+}
+
+function init(){
+    drawDropZone();
+    draw();
+/*     hola(); */
+}
+
+document.addEventListener('DOMContentLoaded', init);
